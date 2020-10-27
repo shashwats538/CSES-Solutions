37e8b5fac8ede06686dcb19b6482d2225b7956ff
shashwat <shashwats538@gmail.com>
2020-10-27 09:14:37 +0530

    Intro Problems

---
+++ Apple_Division.cpp
@@ -0,0 +1,59 @@
+/*There are n apples with known weights. Your task is to divide the apples into two groups so that the difference between the weights of the groups is minimal.
+
+Input
+
+The first input line has an integer n: the number of apples.
+
+The next line has n integers p1,p2,…,pn: the weight of each apple.
+
+Output
+
+Print one integer: the minimum difference between the weights of the groups.
+
+Constraints
+1≤n≤20
+1≤pi≤109
+Example
+
+Input:
+5
+3 2 7 4 1
+
+Output:
+*/
+
+#include <bits/stdc++.h>
+#include <iterator>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#include <math.h>
+
+using namespace std;
+#define vi vector<int>
+#define ll long long
+#define forn(i, n) for (int i = 0; i < n; i++)
+#define len(s) s.length()
+#define MAXN 1000001
+#define MOD 1000000007
+#define fast()                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+void solve()
+{
+}
+
+int main()
+{
+    fast();
+    int t;
+    //cin >> t;
+    t = 1;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
